63c4e7908de9f2afe7268d073aec893926554f62
溝口正樹 <>
2019-11-03 14:50:58 +0900

    init spring boot

---
+++ generalSystem/src/main/java/com/general/system/GeneralSystemApplication.java
@@ -0,0 +1,13 @@
+package com.general.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralSystemApplication.class, args);
+	}
+
+}
---
+++ generalSystem/src/test/java/com/general/system/GeneralSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.general.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
